e1e5da31de96d449ad8630c8f9b28ba6fa6841da
Ali Roozbahani <ali.roozbahani1994@gmail.com>
2026-07-12 18:02:28 +0200

    Initial commit: project setup with Compose, Koin, Room, Maps SDK dependencies

---
+++ app/src/main/java/dev/roozbahani/trailmetrics/MainActivity.kt
@@ -0,0 +1,47 @@
+package dev.roozbahani.trailmetrics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.roozbahani.trailmetrics.ui.theme.TrailMetricsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TrailMetricsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TrailMetricsTheme {
+        Greeting("Android")
+    }
+}
