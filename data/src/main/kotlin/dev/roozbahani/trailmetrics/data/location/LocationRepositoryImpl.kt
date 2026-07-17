4ba12c0be32d686d2a4336c46ec2a3c71d958e4e
Ali Roozbahani <ali.roozbahani1994@gmail.com>
2026-07-17 17:23:14 +0200

    feat(tracking): implement observeLocationUpdates with callbackFlow

--- data/src/main/kotlin/dev/roozbahani/trailmetrics/data/location/LocationRepositoryImpl.kt
+++ data/src/main/kotlin/dev/roozbahani/trailmetrics/data/location/LocationRepositoryImpl.kt
@@ -2,14 +2,23 @@ package dev.roozbahani.trailmetrics.data.location
 
 import android.Manifest
 import android.location.Location
+import android.os.Looper
 import androidx.annotation.RequiresPermission
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationAvailability
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.Priority
 import com.google.android.gms.tasks.CancellationTokenSource
 import dev.roozbahani.trailmetrics.domain.model.Coordinates
+import dev.roozbahani.trailmetrics.domain.model.LocationUpdate
 import dev.roozbahani.trailmetrics.domain.model.RouteError
 import dev.roozbahani.trailmetrics.domain.repository.LocationRepository
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -57,7 +66,60 @@ class LocationRepositoryImpl(
         }
     }
 
-    override fun observeLocationUpdates(): Flow<Coordinates> {
-        TODO("Not yet implemented")
+    @RequiresPermission(
+        anyOf = [
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        ]
+    )
+    override fun observeLocationUpdates(): Flow<LocationUpdate> = callbackFlow {
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            LOCATION_UPDATE_INTERVAL_MILLIS
+        ).build()
+
+        val locationCallback: LocationCallback = object : LocationCallback() {
+            override fun onLocationResult(result: LocationResult) {
+                result.lastLocation?.let {
+                    trySend(
+                        LocationUpdate.Success(
+                            Coordinates(
+                                latitude = it.latitude,
+                                longitude = it.longitude
+                            )
+                        )
+                    )
+                } ?: run {
+                    trySend(LocationUpdate.Unavailable(RouteError.LocationUnavailable()))
+                }
+            }
+
+            override fun onLocationAvailability(availability: LocationAvailability) {
+                if (!availability.isLocationAvailable)
+                    trySend(LocationUpdate.Unavailable(RouteError.LocationUnavailable()))
+            }
+        }
+
+        try {
+            locationProvider.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        } catch (_: SecurityException) {
+            trySend(LocationUpdate.Unavailable(RouteError.MissingLocationPermission()))
+        } catch (ex: CancellationException) {
+            throw ex // propagate to parent scope
+        } catch (ex: Exception) {
+            trySend(LocationUpdate.Unavailable(RouteError.LocationUnavailable(ex)))
+        } finally {
+            awaitClose {
+                locationProvider.removeLocationUpdates(locationCallback)
+            }
+        }
+    }
+
+    companion object {
+        const val LOCATION_UPDATE_INTERVAL_MILLIS = 3_000L
     }
 }
--- domain/src/main/kotlin/dev/roozbahani/trailmetrics/domain/repository/LocationRepository.kt
+++ domain/src/main/kotlin/dev/roozbahani/trailmetrics/domain/repository/LocationRepository.kt
@@ -1,9 +1,10 @@
 package dev.roozbahani.trailmetrics.domain.repository
 
 import dev.roozbahani.trailmetrics.domain.model.Coordinates
+import dev.roozbahani.trailmetrics.domain.model.LocationUpdate
 import kotlinx.coroutines.flow.Flow
 
 interface LocationRepository {
     suspend fun getCurrentLocation(): Result<Coordinates>
-    fun observeLocationUpdates(): Flow<Coordinates>
+    fun observeLocationUpdates(): Flow<LocationUpdate>
 }
