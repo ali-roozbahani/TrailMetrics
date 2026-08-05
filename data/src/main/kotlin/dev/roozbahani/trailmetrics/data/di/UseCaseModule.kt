2009be92182275b198842950a0be18920e39a666
Ali Roozbahani <ali.roozbahani1994@gmail.com>
2026-08-05 18:22:00 +0200

    feat(domain): add SaveActivityUseCase

--- data/src/main/kotlin/dev/roozbahani/trailmetrics/data/di/UseCaseModule.kt
+++ data/src/main/kotlin/dev/roozbahani/trailmetrics/data/di/UseCaseModule.kt
@@ -1,12 +1,23 @@
 package dev.roozbahani.trailmetrics.data.di
 
+import dev.roozbahani.trailmetrics.domain.repository.ActivityHistoryRepository
 import dev.roozbahani.trailmetrics.domain.usecase.GenerateClosedRouteUseCase
 import dev.roozbahani.trailmetrics.domain.usecase.GetCurrentLocationUseCase
+import dev.roozbahani.trailmetrics.domain.usecase.SaveActivityUseCase
 import dev.roozbahani.trailmetrics.domain.usecase.UpdateTrackingStateUseCase
+import dev.roozbahani.trailmetrics.domain.util.CalorieCalculator
+import dev.roozbahani.trailmetrics.domain.util.Clock
 import org.koin.dsl.module
 
 val useCaseModule = module {
     factory { GenerateClosedRouteUseCase(get()) }
     factory { GetCurrentLocationUseCase(get()) }
     factory { UpdateTrackingStateUseCase() }
+    factory {
+        SaveActivityUseCase(
+            activityHistoryRepository = get<ActivityHistoryRepository>(),
+            calorieCalculator = get<CalorieCalculator>(),
+            clock = get<Clock>()
+        )
+    }
 }
